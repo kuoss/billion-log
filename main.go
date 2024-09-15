62b523cbc5e58aba2ca369d263f50f67a08f7b08
jmnote <opcore@gmail.com>
2024-09-15 17:39:02 +0900

    limit concurrency

--- main.go
+++ main.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/google/uuid"
 )
 
 func main() {
+	var wg sync.WaitGroup
+	sem := make(chan struct{}, 100000) // Semaphore to limit concurrency
+
 	for i := 0; i <= 999; i++ {
 		for j := 0; j <= 999; j++ {
 			for k := 0; k <= 999; k++ {
-				go fmt.Printf("I%03d-J%03d-K%03d %s\n", i, j, k, uuid.New())
+				wg.Add(1)
+				sem <- struct{}{} // Acquire a slot
+				go func(i, j, k int) {
+					defer wg.Done()
+					defer func() { <-sem }() // Release the slot
+					fmt.Printf("I%03d-J%03d-K%03d %s\n", i, j, k, uuid.New())
+				}(i, j, k)
 			}
 		}
 	}
+
+	wg.Wait() // Wait for all goroutines to finish
 }
